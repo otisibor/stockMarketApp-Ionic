054de8805da9db3de5c3f2be4b46ff34086be1ae
otisibor <otisibor@gmail.com>
2016-01-06 11:52:20 +0800

    06012016

---
+++ www/js/services.js
@@ -0,0 +1,126 @@
+angular.module('yourAppsName.services',[])
+
+
+.factory('encodeURIservice', function(){
+  return {
+
+      encode: function(string) {
+        console.log(string);
+          return encodeURIComponent(string).replace(/\"/g, "%22").replace(/\ /g, "%20").replace(/[! ' ()]/g, escape);
+  }
+ };
+})
+
+.factory('dateService', function($filter) {
+var currentDate = function() {
+var d = new Date ();
+var date = $filter('date')(d, 'yyyy-MM-dd');
+return date;
+
+};
+
+var oneYearAgoDate = function() {
+  var d = new Date (new Date().setDate(new Date().getDate()-365));
+  var date = $filter('date')(d, 'yyyy-MM-dd');
+  return date;
+
+};
+
+return{
+  currentDate: currentDate,
+  oneYearAgoDate:oneYearAgoDate
+};
+
+})
+
+
+
+
+
+.factory('stockDataService', function($q,$http, encodeURIservice){
+var getDetailsData = function(ticker){
+
+var deferred =$q.defer(),
+
+query = 'select * from yahoo.finance.quotes where symbol IN ("' + ticker + '")',
+url2=' http://query.yahooapis.com/v1/public/yql?q=' + encodeURIservice.encode(query)+'&format=json&env=http://datatables.org/alltables.env';
+console.log (url2);
+
+$http.get(url2)
+.success(function(json) {
+  var jsonData = json.query.results.quote;
+
+   deferred.resolve(jsonData);
+})
+
+.error(function(error){
+console.log("Details data error:"+ error);
+deferred.reject();
+});
+return deferred.promise;
+
+
+
+};
+
+
+var getPriceData = function(ticker){
+
+  var deferred=$q.defer(),
+  url= "http://finance.yahoo.com/webservice/v1/symbols/"+ticker+"/quote?format=json&view=detail";
+
+
+   $http.get(url)
+   .success(function(json) {
+
+  //   console.log(json);
+     var jsonData = json.list.resources[0].resource.fields;
+     deferred.resolve(jsonData);
+  })
+
+.error(function(error){
+  console.log("Price data error:"+ error);
+  deferred.reject();
+ });
+return deferred.promise;
+  };
+
+return {
+  getPriceData:getPriceData,
+  getDetailsData:getDetailsData
+} ;
+})
+
+
+.factory('chartDataService', function($q, $http, encodeURIservice){
+
+var getHistoricalData = function (ticker, fromDate, todayDate) {
+  var deferred= $q.defer(),
+  query= 'select * from yahoo.finance.historicaldata where symbol = "'+ticker+'" and startDate = "'+fromDate+'" and endDate = "'+todayDate+'"';
+  url2=' http://query.yahooapis.com/v1/public/yql?q=' + encodeURIservice.encode(query)+'&format=json&env=http://datatables.org/alltables.env';
+
+  $http.get(url2)
+   .success(function(json) {
+
+     console.log(json) ;
+     var jsonData = json;
+     deferred.resolve(jsonData);
+   })
+.error(function(error){
+ console.log("Chart data error: " +error);
+ deferred.reject();
+
+});
+
+return deferred.promise;
+};
+return{
+  getHistoricalData: getHistoricalData
+};
+
+})
+
+
+
+
+;
